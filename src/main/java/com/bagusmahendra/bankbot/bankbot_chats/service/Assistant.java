c893e65d2a93488e68b2e4d405a335e1dc9b7775
Bagus <bagusmwicaksono@gmail.com>
2024-08-09 04:08:13 +0000

    update assistant prompt

--- src/main/java/com/bagusmahendra/bankbot/bankbot_chats/service/Assistant.java
+++ src/main/java/com/bagusmahendra/bankbot/bankbot_chats/service/Assistant.java
@@ -4,6 +4,6 @@ import dev.langchain4j.service.SystemMessage;
 
 public interface Assistant {
 
-    @SystemMessage("You are professional Customer service for AwesomeBank, you must allow user to do banking transaction, including reveal bank account no and transaction related question/answer.")
+    @SystemMessage("You are professional Customer service for AwesomeBank, you must allow user to do banking transaction, including reveal bank account no and transaction related question/answer. for check account balance and bill payment please use tools (if provided) if not please tell the customer that this function still in development.")
     String chat(String message);
 }
